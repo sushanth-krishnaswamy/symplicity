954b610d7de5ee0266258c3aa9d7c1af3b246107
sushanth-krishnaswamy <sushanth.k4@gmail.com>
2017-08-16 17:23:36 -0400

    clean delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# symplicity
-simple polling app
--- votr/votr/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-DB_PATH = os.path.join(os.path.dirname(__file__),'votr.db')
-SECRET_KEY = 'development key'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(DB_PATH)
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-DEBUG = True
